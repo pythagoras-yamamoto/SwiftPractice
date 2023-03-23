c47cf4a6c2bf41c7dd71f382e3fcf6bf5145b2cb
pythagoras-yamamoto <yamamo8533@gmail.com>
2023-03-23 23:01:51 +0900

    init

---
+++ MyCamera/ImagePickerView.swift
@@ -0,0 +1,14 @@
+//
+//  ImagePickerView.swift
+//  MyCamera
+//
+//  Created by 山本雄大 on 2023/03/23.
+//
+
+import SwiftUI
+
+struct ImagePickerView: UIViewControllerRepresentable {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
--- MyCamera/MyCameraApp.swift
+++ MyCamera/MyCameraApp.swift
@@ -1,10 +1,3 @@
-//
-//  MyCameraApp.swift
-//  MyCamera
-//
-//  Created by 山本雄大 on 2023/03/23.
-//
-
 import SwiftUI
 
 @main
