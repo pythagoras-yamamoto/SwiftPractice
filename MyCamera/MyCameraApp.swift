21772c6301a2db63161763807690c7d10e2a083b
pythagoras-yamamoto <yamamo8533@gmail.com>
2023-03-23 16:06:58 +0900

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by 山本雄大 on 2023/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
